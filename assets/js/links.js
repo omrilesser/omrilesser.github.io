c17096879c3b67ff0c0cd367ee36935ae0d447b8
omrilesser <omrilesser@gmail.com>
2025-01-29 00:34:29 -0500

    links fix

--- assets/js/links.js
+++ assets/js/links.js
@@ -1,12 +1,10 @@
 <script>
 document.addEventListener("DOMContentLoaded", function() {
-  var anchors = document.querySelectorAll('a[href]');
+  const links = document.querySelectorAll("a[href^='http']");
 
-  // Open external links in a new tab
-  anchors.forEach(function(anchor) {
-    if (anchor.hostname !== window.location.hostname) {
-      anchor.setAttribute('target', '_blank');
-      anchor.setAttribute('rel', 'noopener noreferrer');
+  links.forEach(link => {
+    if (link.hostname !== window.location.hostname) {
+      link.target = "_blank";
     }
   });
 });
